1d552bc3dea45c46120d1bd26e8869dd66bce894
analuciabolico <analuciabolico.dev@gmail.com>
2021-12-15 06:30:11 -0300

    add setup project with Spring Boot Framework

---
+++ src/test/java/com/github/analuciabolico/arduinomakerbff/Context.java
@@ -0,0 +1,13 @@
+package com.github.analuciabolico.arduinomakerbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Context {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
