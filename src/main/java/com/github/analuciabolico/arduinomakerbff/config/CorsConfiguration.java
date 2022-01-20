fb0be7e77c3ccc12547f8300629ba28529945f5b
Ana Lucia Bolico <51336956+analuciabolico@users.noreply.github.com>
2022-01-20 20:27:46 -0300

    Add Cors configuration (#7)

---
+++ src/main/java/com/github/analuciabolico/arduinomakerbff/config/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.github.analuciabolico.arduinomakerbff.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@AllArgsConstructor
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://arduino-web.netlify.app/");
+            }
+        };
+    }
+
+}
